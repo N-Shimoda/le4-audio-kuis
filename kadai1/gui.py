c3ee1414ab1acf873a7d79a0fb4d8389c702602b
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2022-12-15 14:49:15 +0900

    deploy speech recognition

--- kadai1/gui.py
+++ kadai1/gui.py
@@ -17,29 +17,50 @@ import tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from src.process import process_data
 
+
+# スライドバーの値が変更されたときに呼び出されるコールバック関数
+# ここで右側のグラフに
+# vはスライドバーの値
+def _tabbar_Cb(v):
+
+  # Acquire index and corresponding data
+  # スライドバーの値からスペクトルのインデクスおよびそのスペクトルを取得
+  index = int((len(spectrogram)-1) * (float(v) / duration))
+  spectrum = spectrogram[index]
+  word = speech[index]
+  
+  # Update spectrogram
+  # 直前のスペクトル描画を削除し，新たなスペクトルを描画
+  plt.cla()
+  x_data = np.fft.rfftfreq(size_frame, d=1/SR)
+  ax2.plot(x_data, spectrum)
+  ax2.set_ylim(-10, 5)
+  ax2.set_xlim(0, SR/2)
+  ax2.set_ylabel('amblitude')
+  ax2.set_xlabel('frequency [Hz]')
+  canvas2.draw()
+
+  # Update speech recognition
+  recognized_word = word
+
+
 #
 # Process sound data
 filename = 'sound/aiueo.wav'
-spectrogram, melody, preference = process_data(filename)
+spectrogram, melody, speech, preference = process_data(filename)
 [SR, size_frame, size_shift, duration] = preference
 
 
 # Tkinterを初期化
 root = tkinter.Tk()
 root.wm_title("EXP4-AUDIO-SAMPLE")
-# root.geometry("1200x800")
 
-# Tkinterのウィジェットを階層的に管理するためにFrameを使用
-# frame1 ... スペクトログラムを表示
-# frame2 ... Scale（スライドバー）とスペクトルを表示
-frame1 = tkinter.Frame(root, bg="beige")
-frame2 = tkinter.Frame(root)
-frame1.pack(side="left")
-frame2.pack(side="right")
-
+#
+# Frame1
+frame1 = tkinter.Frame(root)
 
 # 
-# Spectrogram & Melody
+# Draw spectrogram & Melody
 fig = plt.figure()
 canvas = FigureCanvasTkAgg(fig, master=frame1)	# masterに対象とするframeを指定
 
@@ -60,38 +81,14 @@ ax2.set_ylabel('f0 frequency [Hz]')
 x_data = np.linspace(0, duration, len(melody))
 ax2.plot(x_data, melody, c='y')
 
-canvas.get_tk_widget().pack(side="left")	# 最後にFrameに追加する処理
+canvas.get_tk_widget().pack()
 
 
-# スライドバーの値が変更されたときに呼び出されるコールバック関数
-# ここで右側のグラフに
-# vはスライドバーの値
-def _draw_spectrum(v):
-
-	# スライドバーの値からスペクトルのインデクスおよびそのスペクトルを取得
-	index = int((len(spectrogram)-1) * (float(v) / duration))
-	spectrum = spectrogram[index]
-
-	# 直前のスペクトル描画を削除し，新たなスペクトルを描画
-	plt.cla()
-	x_data = np.fft.rfftfreq(size_frame, d=1/SR)
-	ax2.plot(x_data, spectrum)
-	ax2.set_ylim(-10, 5)
-	ax2.set_xlim(0, SR/2)
-	ax2.set_ylabel('amblitude')
-	ax2.set_xlabel('frequency [Hz]')
-	canvas2.draw()
-
-# スペクトルを表示する領域を確保
-# ax2, canvs2 を使って上記のコールバック関数でグラフを描画する
-fig2, ax2 = plt.subplots()
-canvas2 = FigureCanvasTkAgg(fig2, master=frame2)
-canvas2.get_tk_widget().pack(side="top")	# "top"は上部方向にウィジェットを積むことを意味する
-
-# スライドバーを作成
+# 
+# Create slide bar
 scale = tkinter.Scale(
-	command=_draw_spectrum,		# ここにコールバック関数を指定
-	master=frame2,				# 表示するフレーム
+	command=_tabbar_Cb,		# ここにコールバック関数を指定
+	master=frame1,				# 表示するフレーム
 	from_=0,					# 最小値
 	to=duration,				# 最大値
 	resolution=size_shift/SR,	# 刻み幅
@@ -101,7 +98,28 @@ scale = tkinter.Scale(
 	width=50,					# 縦サイズ
 	font=("", 20)				# フォントサイズは20pxに設定
 )
-scale.pack(side="top")
+scale.pack()
 
+
+#
+# Frame2
+frame2 = tkinter.Frame(root)
+
+# スペクトルを表示する領域を確保
+# ax2, canvs2 を使って上記のコールバック関数でグラフを描画する
+fig2, ax2 = plt.subplots()
+canvas2 = FigureCanvasTkAgg(fig2, master=frame2)
+canvas2.get_tk_widget().pack(side="top")	# "top"は上部方向にウィジェットを積むことを意味する
+
+#
+# Recognized voice
+recognized_word = "あ"
+label = tkinter.Label(frame2, text=recognized_word)
+label.pack()
+
+
+#
 # TkinterのGUI表示を開始
+frame1.pack(side="left")
+frame2.pack(side="right")
 tkinter.mainloop()
--- kadai1/src/process.py
+++ kadai1/src/process.py
@@ -1,13 +1,30 @@
 import librosa
 import numpy as np
+import pickle
 from src.melody_funs import estimate_melody_f0
+from src.speech_model import recognize_word
+
+# 
+# Load parameters of speech recognition
+with open("ex16/mu_sigma_result.pickle", mode="rb") as f:
+
+  mu_sigma_result = pickle.load(f)
+
+  # preference of frame
+  dim = mu_sigma_result[4][0]
+  size_frame = mu_sigma_result[4][1]
+  size_shift = mu_sigma_result[4][2]
+
+  print("preference: {}".format(mu_sigma_result[4]))
 
 
 def process_data(filename):
 
+  # preference of frame
   size_frame = 4096	# フレームサイズ
   SR = 16000			# サンプリングレート
   size_shift = 16000 / 100	# シフトサイズ = 0.001 秒 (10 msec)
+  hamming_window = np.hamming(size_frame)   # ハミング窓
 
   # 音声ファイルを読み込む
   x, _ = librosa.load(filename, sr=SR)
@@ -15,29 +32,43 @@ def process_data(filename):
   # ファイルサイズ（秒）
   duration = len(x) / SR
 
-  # ハミング窓
-  hamming_window = np.hamming(size_frame)
-
-  # スペクトログラムを保存するlist
+  # lists for storing result
   spectrogram = []
   melody = []
+  speech = []
+  preference = [SR, size_frame, size_shift, duration]
 
-  # フレーム毎にスペクトルを計算
+
+  # divide data into frames
   for i in np.arange(0, len(x)-size_frame, size_shift):
     # 該当フレームのデータを取得
     idx = int(i)	# arangeのインデクスはfloatなのでintに変換
     x_frame = x[idx : idx+size_frame]
     
-    # スペクトル
+    #
+    # Spectrum
     fft_spec = np.fft.rfft(x_frame * hamming_window)
     fft_log_abs_spec = np.log(np.abs(fft_spec))
     spectrogram.append(fft_log_abs_spec)
     
+    #
     # Estimate frequency of melody
     nn_range = np.arange(36,61,0.1)   # range of note number
     _, melody_f0 = estimate_melody_f0(nn_range, fft_log_abs_spec, SR)
     melody.append(melody_f0)
 
-  preference = [SR, size_frame, size_shift, duration]
+    #
+    # Speech recognition
+    # Calculate cepstrum
+    amp_spectrum = np.fft.rfft(x_frame)
+    log_abs_spectrum = np.log( np.abs(amp_spectrum) )
+    cepstrum = np.abs( np.fft.fft(log_abs_spectrum) )
 
-  return spectrogram, melody, preference
+    # extract cepstrum of low index (0~dim & -dim~)
+    # cepstrum = np.concatenate([cepstrum[:dim], cepstrum[-dim:]])
+    cepstrum = cepstrum[:dim]
+
+    speech.append( recognize_word(cepstrum) )
+
+
+  return spectrogram, melody, speech, preference
