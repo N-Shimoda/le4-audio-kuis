c8bcb8357fe50a37f5c6bc531cea01bd97a0556c
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2022-12-28 19:25:16 +0900

    add report1

--- kadai2/gui.py
+++ kadai2/gui.py
@@ -1,20 +1,34 @@
 import tkinter as tk
 
+# class 'Application' inherits tk.Frame
 class Application(tk.Frame):
 
   def __init__(self, master=None):
-    super().__init__(master)
-    self.pack()
+    super().__init__(master, width=1200, height=800)
+    self["bg"]="black"
+    self.pack(expand=True, fill="both")
 
     # Frames
     self.frame_top = tk.Frame(self, bd=2, relief="raised")
     self.frame_left = tk.Frame(self, bd=2, relief="raised")
     self.frame_right = tk.Frame(self, bd=2, relief="raised")
-    self.frame_top.pack(side="top", expand=True, fill="both")
-    self.frame_left.pack(side="left", expand=True, fill="both")
-    self.frame_right.pack(side="right", expand=True, fill="both")
+    """
+    self.frame_top.place(width=self["width"], height=10)
+    self.frame_left.place(width=self["width"]//2, height=self["height"]-10)
+    self.frame_right.place(width=self["width"]//2, height=self["height"]-10)
+    """
+    self.frame_top.pack(side="top", anchor="n", expand=True, fill="x")
+    self.frame_left.pack(side="left", anchor="s", expand=True, fill="both")
+    self.frame_right.pack(side="right", anchor="n", expand=True, fill="both")
+    
+    """
+    self.frame_top.grid(column=0, row=0, columnspan=2, sticky="we")
+    self.frame_left.grid(column=0, row=1)
+    self.frame_right.grid(column=1, row=1)
+    """
 
     self.create_widgets()
+
 
   def create_widgets(self):
     # Frame TOP
